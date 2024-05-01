3b0737abf2a7f581e27b564d64f3d0fbc85c38f2
Егор Савин <savinwho@gmail.com>
2024-05-01 16:08:56 +0300

    анимашки

--- Calendar/View/PageCalendar.xaml.cs
+++ Calendar/View/PageCalendar.xaml.cs
@@ -9,20 +9,54 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Calendar
 {
-    /// <summary>
-    /// Логика взаимодействия для PageCalendar.xaml
-    /// </summary>
     public partial class PageCalendar : Page
     {
         public PageCalendar()
         {
             InitializeComponent();
+        }
+
+        public object BackButton { get; private set; }
+
+        private void LastMonth_Click(object sender, RoutedEventArgs e)
+        {
+            ColorAnimation colorAnim = new ColorAnimation();
+            colorAnim.From = ((SolidColorBrush)LastMonthBt.Background).Color;
+            colorAnim.To = Colors.White;
+            colorAnim.FillBehavior = FillBehavior.Stop;
+            colorAnim.Duration = TimeSpan.FromSeconds(1);
+            LastMonthBt.Background.BeginAnimation(SolidColorBrush.ColorProperty, colorAnim);
+
+            ColorAnimation colorTextAnim = new ColorAnimation();
+            colorTextAnim.From = ((SolidColorBrush)LastMonthBt.Foreground).Color;
+            colorTextAnim.To = Colors.Red;
+            colorTextAnim.FillBehavior = FillBehavior.Stop;
+            colorTextAnim.Duration = TimeSpan.FromSeconds(1);
+            LastMonthBt.Foreground.BeginAnimation(SolidColorBrush.ColorProperty, colorAnim);
+        }
+
+        private void NextMonth_Click(object sender, RoutedEventArgs e)
+        {
+            ColorAnimation colorAnim = new ColorAnimation();
+            colorAnim.From = ((SolidColorBrush)NextMonthBt.Background).Color;
+            colorAnim.To = Colors.White;
+            colorAnim.FillBehavior = FillBehavior.Stop;
+            colorAnim.Duration = TimeSpan.FromSeconds(1);
+            NextMonthBt.Background.BeginAnimation(SolidColorBrush.ColorProperty, colorAnim);
+
+            ColorAnimation colorTextAnim = new ColorAnimation();
+            colorTextAnim.From = ((SolidColorBrush)NextMonthBt.Foreground).Color;
+            colorTextAnim.To = Colors.Red;
+            colorTextAnim.FillBehavior = FillBehavior.Stop;
+            colorTextAnim.Duration = TimeSpan.FromSeconds(1);
+            NextMonthBt.Foreground.BeginAnimation(SolidColorBrush.ColorProperty, colorAnim);
         }
     }
 }
--- Calendar/View/PageDay.xaml.cs
+++ Calendar/View/PageDay.xaml.cs
@@ -9,20 +9,52 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Calendar
 {
-    /// <summary>
-    /// Логика взаимодействия для PageDay.xaml
-    /// </summary>
     public partial class PageDay : Page
     {
         public PageDay()
         {
             InitializeComponent();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            ColorAnimation colorAnim = new ColorAnimation();
+            colorAnim.From = ((SolidColorBrush)BackButton.Background).Color;
+            colorAnim.To = Colors.White;
+            colorAnim.FillBehavior = FillBehavior.Stop;
+            colorAnim.Duration = TimeSpan.FromSeconds(1);  
+            BackButton.Background.BeginAnimation(SolidColorBrush.ColorProperty, colorAnim);
+
+            ColorAnimation colorTextAnim = new ColorAnimation();
+            colorTextAnim.From = ((SolidColorBrush)BackButton.Foreground).Color;
+            colorTextAnim.To = Colors.Red;
+            colorTextAnim.FillBehavior = FillBehavior.Stop;
+            colorTextAnim.Duration = TimeSpan.FromSeconds(1);
+            BackButton.Foreground.BeginAnimation(SolidColorBrush.ColorProperty, colorAnim);
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            ColorAnimation colorAnim = new ColorAnimation();
+            colorAnim.From = ((SolidColorBrush)BackButton.Background).Color;
+            colorAnim.To = Colors.White;
+            colorAnim.FillBehavior = FillBehavior.Stop;
+            colorAnim.Duration = TimeSpan.FromSeconds(1);
+            BackButton.Background.BeginAnimation(SolidColorBrush.ColorProperty, colorAnim);
+
+            ColorAnimation colorTextAnim = new ColorAnimation();
+            colorTextAnim.From = ((SolidColorBrush)BackButton.Foreground).Color;
+            colorTextAnim.To = Colors.Red;
+            colorTextAnim.FillBehavior = FillBehavior.Stop;
+            colorTextAnim.Duration = TimeSpan.FromSeconds(1);
+            BackButton.Foreground.BeginAnimation(SolidColorBrush.ColorProperty, colorAnim);
         }
     }
 }
